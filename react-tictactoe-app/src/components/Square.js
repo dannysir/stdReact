71b37bb679c7af6bb7f8baebceacd3f42a483005
Seo San <dannysir@naver.com>
2024-04-18 16:41:03 +0900

    Refactor : React hook 함수형 모듈로 변경 후, hook을 이용.

--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './Square.css';
-export default class Square extends Component {
 
-    render() {
-        return (
-            <button className="square"
-                    onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+const Square = ({value, onClick}) => {
+
+    return (
+        <button className="square"
+                onClick={() => onClick()}>
+            {value}
+        </button>
+    );
 }
+export default Square;
