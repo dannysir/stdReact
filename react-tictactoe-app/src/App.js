fb94ca69312737626b4797c1bb5d8a1eed3da4f1
Seo San <dannysir@naver.com>
2024-04-18 18:12:11 +0900

    Refactor : 시간여행 기능을 위한 위치 변경

--- react-tictactoe-app/src/App.js
+++ react-tictactoe-app/src/App.js
@@ -1,16 +1,65 @@
 import "./App.css"
 import Board from "./components/Board";
+import React, {useState} from "react";
+
 function App() {
-  return (
-      <div className="game">
-        <div className="game-board">
-          <Board/>
+    const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+    const [isX, setIsX] = useState(true);
+    const calculateWinner = (squares) => {
+        const LINES = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < LINES.length; i++) {
+            const [a, b, c] = LINES[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    let current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+
+    if (winner) {
+        status = 'Winner : ' + winner;
+    } else {
+        status = `Next player ${isX ? 'X' : `O`}`;
+    }
+
+    const handleClick = (i) => {
+        const newSquares = current.squares.slice();
+        if (calculateWinner(newSquares) || newSquares[i]) {
+            return;
+        }
+        newSquares[i] = isX ? 'X' : 'O';
+        setHistory([...history, {squares: newSquares}]);
+
+        setIsX(prev => !prev);
+    };
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares = {current.squares}
+                    onClick = {(i) => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div className="status">{status}</div>
+            </div>
         </div>
-        <div className="game-info">
-          game-info
-        </div>
-      </div>
-  );
+    );
 }
 
 export default App;
--- react-tictactoe-app/src/components/Board.js
+++ react-tictactoe-app/src/components/Board.js
@@ -1,79 +1,39 @@
 import React, {useState} from 'react';
 import Square from "./Square";
 import './Board.css';
-const Board = () => {
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isX, setIsX] = useState(true);
-    const calculateWinner = (squares) => {
-        const LINES = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        for (let i = 0; i < LINES.length; i++) {
-            const [a, b, c] = LINES[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    };
+const Board = ({squares, onClick}) => {
 
-    const winner = calculateWinner(squares);
-    let status;
-
-    if (winner) {
-        status = 'Winner : ' + winner;
-    } else {
-        status = `Next player ${isX ? 'X' : `O`}`;
-    }
-
-    const handleClick = (i) => {
-        const newSquares = squares.slice();
-        if (calculateWinner(newSquares) || newSquares[i]) {
-            return;
-        }
-        newSquares[i] = isX ? 'X' : 'O';
-        setSquares(newSquares);
-        setIsX(prev => !prev);
-    };
 
 
     const renderSquare = (i) => {
         return <Square
             value={squares[i]}
-            onClick={() => handleClick(i)}
+            onClick={() => onClick(i)}
         />;
     }
 
 
     return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {renderSquare(0)}
-                    {renderSquare(1)}
-                    {renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {renderSquare(3)}
-                    {renderSquare(4)}
-                    {renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {renderSquare(6)}
-                    {renderSquare(7)}
-                    {renderSquare(8)}
-                </div>
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
     );
 
-}
+};
 
 export default Board;
