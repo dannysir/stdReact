7d7e66ec31ecf7bcc53273b63923603edabf2a49
Seo San <dannysir@naver.com>
2024-04-19 17:27:37 +0900

    test : 컴포넌트 세분화 효율성 테스트

--- react-performance-app/src/components/B.js
+++ react-performance-app/src/components/B.js
@@ -1,9 +1,32 @@
 import React from "react";
-const B = () => {
+
+const Message = ({message}) => {
+    return <p>{message}</p>;
+};
+const ListItem = ({post}) =>{
+    return (
+        <li>
+            <p>{post.title}</p>
+        </li>
+    )
+}
+const List = ({post}) => {
+    return (
+        <ul>
+            {post.map(v => (
+                <ListItem key={v.id} post={v}/>
+            ))}
+        </ul>
+    );
+};
+const B = ({message, posts}) => {
     return (
         <div>
-            B
+            <h1>B Component</h1>
+            <p>{message}</p>
+            <List post={posts}/>
+            <Message message={message}/>
         </div>
-    )
+    );
 }
 export default B;
