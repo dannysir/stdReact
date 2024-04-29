f2df466591d28264c78fcfc161d168d7efef79f6
Seo San <dannysir@naver.com>
2024-04-29 14:05:11 +0900

    detail page 검색 에러 발생

--- react-disney-app/src/pages/DetailPage/index.js
+++ react-disney-app/src/pages/DetailPage/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import axios from "../../api/axios";
 
 const DetailPage = () => {
-    return (
-        <div>
+    let {movieId} = useParams();
+    const [movie, setMovie] = useState({});
 
-        </div>
-    )
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`/movie/${movieId}`);
+                // console.log('response', response);
+                setMovie(response.data);
+            } catch (error) {
+                console.error("에러가 났네", error);
+            }
+        }
+        fetchData();
+    }, [movieId]);
+
+    if (!movie || Object.keys(movie).length === 0) return null;
+
+    return (
+        <section>
+            <img className='modal__poster-img'
+                 src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                 alt="img"
+            />
+        </section>
+    );
 };
 
 export default DetailPage;
